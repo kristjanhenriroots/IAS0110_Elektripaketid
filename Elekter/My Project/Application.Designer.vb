b5b6eca8bd4276ce54369f702bc9fc1aab7cc358
kristjanhenriroots <85015619+kristjanhenriroots@users.noreply.github.com>
2023-03-28 19:42:45 +0300

    GIGA monolith

--- Elekter/My Project/Application.Designer.vb
+++ Elekter/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Elekter.Form1
+            Me.MainForm = Global.Elekter.MainForm
         End Sub
     End Class
 End Namespace
