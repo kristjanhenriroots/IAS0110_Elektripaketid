82c5861d44471ff912513e6e5e6854a997ddc209
Johhny2000 <57295117+Johhny2000@users.noreply.github.com>
2023-03-31 17:04:56 +0300

    formCalc almost finished, missing input from börss

--- testFormid/testKustudadaParast/formCalc.Designer.vb
+++ testFormid/testKustudadaParast/formCalc.Designer.vb
@@ -22,161 +22,180 @@ Partial Class formCalc
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.electricityRateTextBox = New System.Windows.Forms.TextBox()
+        Me.timeUsedTextBox = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.Button9 = New System.Windows.Forms.Button()
         Me.Button8 = New System.Windows.Forms.Button()
         Me.btn_home = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.costTextBox = New System.Windows.Forms.TextBox()
+        Me.powerRatingTextBox = New System.Windows.Forms.TextBox()
+        Me.calculateButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.applianceComboBox = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(200, 69)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(178, 28)
-        Me.ComboBox1.TabIndex = 0
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(38, 69)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(156, 20)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Vali tüüpkodumasin: "
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(200, 213)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(100, 28)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Kuva"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(38, 104)
+        Me.Label2.Location = New System.Drawing.Point(317, 182)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(92, 20)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Vali pakett: "
+        Me.Label2.Size = New System.Drawing.Size(98, 20)
+        Me.Label2.TabIndex = 52
+        Me.Label2.Text = "autofill börss"
         '
-        'ComboBox2
+        'Label6
         '
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(200, 104)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(178, 28)
-        Me.ComboBox2.TabIndex = 4
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(43, 179)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(137, 20)
+        Me.Label6.TabIndex = 51
+        Me.Label6.Text = "Paketihind (kWh): "
         '
-        'TextBox1
+        'Label5
         '
-        Me.TextBox1.Location = New System.Drawing.Point(200, 138)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
-        Me.TextBox1.TabIndex = 5
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(26, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(154, 20)
+        Me.Label5.TabIndex = 50
+        Me.Label5.Text = "Kasutamise aeg (h): "
         '
-        'TextBox2
+        'electricityRateTextBox
         '
-        Me.TextBox2.Location = New System.Drawing.Point(200, 170)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 26)
-        Me.TextBox2.TabIndex = 6
+        Me.electricityRateTextBox.Location = New System.Drawing.Point(202, 176)
+        Me.electricityRateTextBox.Name = "electricityRateTextBox"
+        Me.electricityRateTextBox.Size = New System.Drawing.Size(100, 26)
+        Me.electricityRateTextBox.TabIndex = 49
         '
-        'Label3
+        'timeUsedTextBox
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(38, 138)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(133, 20)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Tarbimisvõimsus: "
+        Me.timeUsedTextBox.Location = New System.Drawing.Point(202, 139)
+        Me.timeUsedTextBox.Name = "timeUsedTextBox"
+        Me.timeUsedTextBox.Size = New System.Drawing.Size(100, 26)
+        Me.timeUsedTextBox.TabIndex = 48
         '
-        'Label4
+        'Button3
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(38, 170)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(133, 20)
-        Me.Label4.TabIndex = 8
-        Me.Label4.Text = "Orienteeruv hind: "
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(322, 149)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(133, 41)
-        Me.Button2.TabIndex = 9
-        Me.Button2.Text = "Muuda andmeid"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button3.Location = New System.Drawing.Point(247, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 30)
+        Me.Button3.TabIndex = 47
+        Me.Button3.Text = "Otsing"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Button9
         '
-        Me.Button9.Location = New System.Drawing.Point(164, 1)
+        Me.Button9.Location = New System.Drawing.Point(166, 3)
         Me.Button9.Name = "Button9"
         Me.Button9.Size = New System.Drawing.Size(75, 30)
-        Me.Button9.TabIndex = 30
+        Me.Button9.TabIndex = 46
         Me.Button9.Text = "Võrd"
         Me.Button9.UseVisualStyleBackColor = True
         '
         'Button8
         '
-        Me.Button8.Location = New System.Drawing.Point(83, 1)
+        Me.Button8.Location = New System.Drawing.Point(85, 3)
         Me.Button8.Name = "Button8"
         Me.Button8.Size = New System.Drawing.Size(75, 30)
-        Me.Button8.TabIndex = 29
+        Me.Button8.TabIndex = 45
         Me.Button8.Text = "Kalk"
         Me.Button8.UseVisualStyleBackColor = True
         '
         'btn_home
         '
-        Me.btn_home.Location = New System.Drawing.Point(2, 1)
+        Me.btn_home.Location = New System.Drawing.Point(4, 3)
         Me.btn_home.Name = "btn_home"
         Me.btn_home.Size = New System.Drawing.Size(75, 30)
-        Me.btn_home.TabIndex = 28
+        Me.btn_home.TabIndex = 44
         Me.btn_home.Text = "Home"
         Me.btn_home.UseVisualStyleBackColor = True
         '
-        'Button3
+        'Label4
         '
-        Me.Button3.Location = New System.Drawing.Point(245, 1)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 30)
-        Me.Button3.TabIndex = 31
-        Me.Button3.Text = "Otsing"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(47, 216)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(133, 20)
+        Me.Label4.TabIndex = 43
+        Me.Label4.Text = "Orienteeruv hind: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(162, 20)
+        Me.Label3.TabIndex = 42
+        Me.Label3.Text = "Tarbimisvõimsus (W): "
+        '
+        'costTextBox
+        '
+        Me.costTextBox.Location = New System.Drawing.Point(202, 213)
+        Me.costTextBox.Name = "costTextBox"
+        Me.costTextBox.Size = New System.Drawing.Size(100, 26)
+        Me.costTextBox.TabIndex = 41
+        '
+        'powerRatingTextBox
+        '
+        Me.powerRatingTextBox.Location = New System.Drawing.Point(202, 102)
+        Me.powerRatingTextBox.Name = "powerRatingTextBox"
+        Me.powerRatingTextBox.Size = New System.Drawing.Size(100, 26)
+        Me.powerRatingTextBox.TabIndex = 40
+        '
+        'calculateButton
+        '
+        Me.calculateButton.Location = New System.Drawing.Point(202, 260)
+        Me.calculateButton.Name = "calculateButton"
+        Me.calculateButton.Size = New System.Drawing.Size(100, 35)
+        Me.calculateButton.TabIndex = 39
+        Me.calculateButton.Text = "Kuva"
+        Me.calculateButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 20)
+        Me.Label1.TabIndex = 38
+        Me.Label1.Text = "Vali tüüpkodumasin: "
+        '
+        'applianceComboBox
+        '
+        Me.applianceComboBox.FormattingEnabled = True
+        Me.applianceComboBox.Location = New System.Drawing.Point(202, 67)
+        Me.applianceComboBox.Name = "applianceComboBox"
+        Me.applianceComboBox.Size = New System.Drawing.Size(227, 28)
+        Me.applianceComboBox.TabIndex = 37
         '
         'formCalc
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(480, 270)
+        Me.ClientSize = New System.Drawing.Size(523, 343)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.electricityRateTextBox)
+        Me.Controls.Add(Me.timeUsedTextBox)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button9)
         Me.Controls.Add(Me.Button8)
         Me.Controls.Add(Me.btn_home)
-        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.ComboBox2)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.costTextBox)
+        Me.Controls.Add(Me.powerRatingTextBox)
+        Me.Controls.Add(Me.calculateButton)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.applianceComboBox)
         Me.Name = "formCalc"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Rakendus_Kalkulaator"
@@ -185,18 +204,20 @@ Partial Class formCalc
 
     End Sub
 
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Button1 As Button
     Friend WithEvents Label2 As Label
-    Friend WithEvents ComboBox2 As ComboBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Button2 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents electricityRateTextBox As TextBox
+    Friend WithEvents timeUsedTextBox As TextBox
+    Friend WithEvents Button3 As Button
     Friend WithEvents Button9 As Button
     Friend WithEvents Button8 As Button
     Friend WithEvents btn_home As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents costTextBox As TextBox
+    Friend WithEvents powerRatingTextBox As TextBox
+    Friend WithEvents calculateButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents applianceComboBox As ComboBox
 End Class
--- testFormid/testKustudadaParast/formCalc.vb
+++ testFormid/testKustudadaParast/formCalc.vb
@@ -1,3 +1,76 @@
 ﻿Public Class formCalc
 
+    Private appliances As Dictionary(Of String, Double)
+
+    Private Sub formCalc_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Set combobox dropdown style so user can't write'
+        applianceComboBox.DropDownStyle = ComboBoxStyle.DropDownList
+
+        'Disable calculateButton until all required entries are filled'
+        calculateButton.Enabled = False
+
+        'Set costTextBox to read-only'
+        costTextBox.ReadOnly = True
+
+        'Fill appliances dictionary'
+        'Data from: https://rohe.geenius.ee/rubriik/uudis/millised-su-kodumasinad-kulutavad-aastas-enim-elektrit-ei-see-pole-pesumasin-voi-elektripliit/'
+        appliances = New Dictionary(Of String, Double)
+        appliances.Add("Mikrolaineahi [1000W]", 1000)
+        appliances.Add("Kohvimasin [1000 W]", 1000)
+        appliances.Add("Veekeetja [1500 W]", 1500)
+        appliances.Add("Keraamiline pliit [2000 W]", 2000)
+        appliances.Add("Induktsioonpliit [2500 W]", 2500)
+        appliances.Add("Elektripliit [2500 W]", 2500)
+        appliances.Add("Elektriahi [3000 W]", 3000)
+        appliances.Add("Triikraud [2000 W]", 2000)
+        appliances.Add("Tolmuimeja [800 W]", 800)
+        appliances.Add("Mängukonsool []", 0)
+        appliances.Add("Digiboks []", 0)
+        appliances.Add("Wifi ruuter []", 0)
+        appliances.Add("Kell-raadio [10 W]", 10)
+        appliances.Add("Elektriboiler 100 l [2200 W]", 2200)
+        appliances.Add("Elektriradiaator [2000 W]", 2000)
+        appliances.Add("Valgusti Halogeen [60 W]", 60)
+        appliances.Add("Valgusti LED [10 W]", 10)
+        appliances.Add("Laptop [50 W]", 50)
+        appliances.Add("Arvuti [250 W]", 250)
+        appliances.Add("Televisioon [100 W]", 100)
+
+        For Each applianceName As String In appliances.Keys
+            applianceComboBox.Items.Add(applianceName)
+        Next
+    End Sub
+
+    Private Sub applianceComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles applianceComboBox.SelectedIndexChanged
+        ' When the user selects an appliance from the ComboBox, update the powerRatingTextBox with its power rating.
+        Dim selectedAppliance As String = applianceComboBox.SelectedItem.ToString()
+        Dim powerRating As Double = appliances(selectedAppliance)
+        powerRatingTextBox.Text = powerRating.ToString()
+    End Sub
+
+    Private Sub anyTextBox_TextChanged(sender As Object, e As EventArgs) Handles powerRatingTextBox.TextChanged, electricityRateTextBox.TextChanged, timeUsedTextBox.TextChanged
+        ' Enable the calculateButton if all inputs are entered.
+        If Not String.IsNullOrEmpty(powerRatingTextBox.Text) And Not String.IsNullOrEmpty(timeUsedTextBox.Text) And Not String.IsNullOrEmpty(electricityRateTextBox.Text) Then
+            calculateButton.Enabled = True
+        Else
+            calculateButton.Enabled = False
+        End If
+    End Sub
+
+    Private Sub calculateButton_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+        Dim powerRating As Double
+        Dim timeUsed As Double
+        Dim electricityRate As Double
+        Dim cost As Double
+
+        powerRating = Double.Parse(powerRatingTextBox.Text)
+        timeUsed = Double.Parse(timeUsedTextBox.Text)
+        electricityRate = Double.Parse(electricityRateTextBox.Text)
+
+        cost = (powerRating / 1000) * timeUsed * electricityRate
+
+        costTextBox.Text = cost.ToString()
+    End Sub
+
+
 End Class
