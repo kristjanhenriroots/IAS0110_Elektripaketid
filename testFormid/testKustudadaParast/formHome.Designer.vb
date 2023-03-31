5dba317a61f268f01700f315a982848e4710914d
Johhny2000 <57295117+Johhny2000@users.noreply.github.com>
2023-03-31 18:40:45 +0300

    added enlarge and shrink buttons to formHome

--- testFormid/testKustudadaParast/formHome.Designer.vb
+++ testFormid/testKustudadaParast/formHome.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class formHome
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class formHome
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(formHome))
         Me.Label1 = New System.Windows.Forms.Label()
@@ -28,8 +28,8 @@ Partial Class formHome
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.enlargeButton = New System.Windows.Forms.Button()
+        Me.shrinkButton = New System.Windows.Forms.Button()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
@@ -61,7 +61,7 @@ Partial Class formHome
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(31, 73)
+        Me.Label2.Location = New System.Drawing.Point(28, 48)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(242, 20)
         Me.Label2.TabIndex = 1
@@ -70,7 +70,7 @@ Partial Class formHome
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(35, 106)
+        Me.PictureBox1.Location = New System.Drawing.Point(32, 81)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(535, 288)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -81,7 +81,7 @@ Partial Class formHome
         '
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {"Tulpdiagramm", "Tabeldiagramm"})
-        Me.ComboBox1.Location = New System.Drawing.Point(267, 421)
+        Me.ComboBox1.Location = New System.Drawing.Point(264, 405)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(124, 28)
         Me.ComboBox1.TabIndex = 4
@@ -89,34 +89,34 @@ Partial Class formHome
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(263, 401)
+        Me.Label3.Location = New System.Drawing.Point(260, 385)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(108, 20)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "Graafiku valik:"
         '
-        'Button4
+        'enlargeButton
         '
-        Me.Button4.Location = New System.Drawing.Point(717, 396)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(25, 25)
-        Me.Button4.TabIndex = 8
-        Me.Button4.Text = "+"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.enlargeButton.Location = New System.Drawing.Point(717, 396)
+        Me.enlargeButton.Name = "enlargeButton"
+        Me.enlargeButton.Size = New System.Drawing.Size(25, 25)
+        Me.enlargeButton.TabIndex = 8
+        Me.enlargeButton.Text = "+"
+        Me.enlargeButton.UseVisualStyleBackColor = True
         '
-        'Button5
+        'shrinkButton
         '
-        Me.Button5.Location = New System.Drawing.Point(717, 427)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(25, 25)
-        Me.Button5.TabIndex = 9
-        Me.Button5.Text = "-"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.shrinkButton.Location = New System.Drawing.Point(717, 427)
+        Me.shrinkButton.Name = "shrinkButton"
+        Me.shrinkButton.Size = New System.Drawing.Size(25, 25)
+        Me.shrinkButton.TabIndex = 9
+        Me.shrinkButton.Text = "-"
+        Me.shrinkButton.UseVisualStyleBackColor = True
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(568, 239)
+        Me.Label4.Location = New System.Drawing.Point(565, 214)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(182, 20)
         Me.Label4.TabIndex = 11
@@ -124,21 +124,21 @@ Partial Class formHome
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(572, 262)
+        Me.TextBox1.Location = New System.Drawing.Point(573, 237)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(170, 26)
         Me.TextBox1.TabIndex = 12
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(35, 422)
+        Me.TextBox2.Location = New System.Drawing.Point(32, 406)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.Size = New System.Drawing.Size(100, 26)
         Me.TextBox2.TabIndex = 13
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(161, 422)
+        Me.TextBox3.Location = New System.Drawing.Point(158, 406)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.Size = New System.Drawing.Size(100, 26)
         Me.TextBox3.TabIndex = 14
@@ -146,7 +146,7 @@ Partial Class formHome
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(31, 401)
+        Me.Label5.Location = New System.Drawing.Point(28, 385)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(122, 20)
         Me.Label5.TabIndex = 15
@@ -155,7 +155,7 @@ Partial Class formHome
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(141, 425)
+        Me.Label6.Location = New System.Drawing.Point(138, 409)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(14, 20)
         Me.Label6.TabIndex = 16
@@ -164,7 +164,7 @@ Partial Class formHome
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(349, 73)
+        Me.Label7.Location = New System.Drawing.Point(346, 48)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(221, 20)
         Me.Label7.TabIndex = 19
@@ -173,7 +173,7 @@ Partial Class formHome
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(400, 401)
+        Me.Label8.Location = New System.Drawing.Point(397, 385)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(97, 20)
         Me.Label8.TabIndex = 20
@@ -181,7 +181,7 @@ Partial Class formHome
         '
         'TextBox4
         '
-        Me.TextBox4.Location = New System.Drawing.Point(397, 422)
+        Me.TextBox4.Location = New System.Drawing.Point(394, 406)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.Size = New System.Drawing.Size(170, 26)
         Me.TextBox4.TabIndex = 21
@@ -189,7 +189,7 @@ Partial Class formHome
         'ComboBox2
         '
         Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(580, 129)
+        Me.ComboBox2.Location = New System.Drawing.Point(577, 104)
         Me.ComboBox2.Name = "ComboBox2"
         Me.ComboBox2.Size = New System.Drawing.Size(121, 28)
         Me.ComboBox2.TabIndex = 22
@@ -197,7 +197,7 @@ Partial Class formHome
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(576, 106)
+        Me.Label9.Location = New System.Drawing.Point(573, 81)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(82, 20)
         Me.Label9.TabIndex = 23
@@ -205,7 +205,7 @@ Partial Class formHome
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(580, 163)
+        Me.Button3.Location = New System.Drawing.Point(577, 138)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(121, 56)
         Me.Button3.TabIndex = 24
@@ -269,8 +269,8 @@ Partial Class formHome
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.shrinkButton)
+        Me.Controls.Add(Me.enlargeButton)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.PictureBox1)
@@ -290,8 +290,8 @@ Partial Class formHome
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
+    Friend WithEvents enlargeButton As Button
+    Friend WithEvents shrinkButton As Button
     Friend WithEvents Label4 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
--- testFormid/testKustudadaParast/formHome.vb
+++ testFormid/testKustudadaParast/formHome.vb
@@ -10,4 +10,32 @@
         formVordlus.Show()
         Me.Hide()
     End Sub
+
+    'Size buttons
+    Private Sub enlargeButton_Click(sender As Object, e As EventArgs) Handles enlargeButton.Click
+        'Increase the size of the form and all its elements by 5%
+        Me.Width *= 1.05
+        Me.Height *= 1.05
+
+        For Each ctrl As Control In Me.Controls
+            ctrl.Width *= 1.05
+            ctrl.Height *= 1.05
+            ctrl.Left *= 1.05
+            ctrl.Top *= 1.05
+        Next
+    End Sub
+
+    Private Sub shrink_Click(sender As Object, e As EventArgs) Handles shrinkButton.Click
+        'Increase the size of the form and all its elements by 5%
+        Me.Width *= 0.95
+        Me.Height *= 0.95
+
+        For Each ctrl As Control In Me.Controls
+            ctrl.Width *= 0.95
+            ctrl.Height *= 0.95
+            ctrl.Left *= 0.95
+            ctrl.Top *= 0.95
+        Next
+    End Sub
+
 End Class
