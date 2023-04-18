634924b65f6a0f88ab973795d57ceda4479b5dbe
Johhny2000 <57295117+Johhny2000@users.noreply.github.com>
2023-04-18 15:24:19 +0300

    Added updates from branch martin_backup, fixed bug with chart.

--- Elekter/formVordlus.vb
+++ Elekter/formVordlus.vb
@@ -1,14 +1,33 @@
 ﻿Public Class formVordlus
+
+    Private deals As Dictionary(Of String, Double)
     Private Sub formVordlus_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Items for pakettCheckedListBox
-        pakettCheckedListBox.Items.Add("Pakett 1")
-        pakettCheckedListBox.Items.Add("Pakett 2")
-        pakettCheckedListBox.Items.Add("Börss")
+        deals = New Dictionary(Of String, Double) From {
+            {"Börss", MainForm.ReturnCurrentPrice},
+            {"Universaal", 19.95}, 'https://www.energia.ee/et/era/elekter/elektrileping-ja-paketid?customers=home-customer&packages=fixPlus
+            {"Kindel 6", 15.89},
+            {"Kindel 36", 17.5},
+            {"Kindel Pluss", 14.49},
+            {"Kasulik Klõps", 16.73}, 'https://elektrihind.ee/paketid/
+            {"Tähtajaline fiseeritud", 13.5},
+            {"Kindel", 13.57},
+            {"Tähtajaline fikseeritud hind + ühisarve", 13.96}
+        }
+
+        'Go through each deal in deals dictionary and them to pakettCheckedListBox.
+        For Each dealName As String In deals.Keys
+            pakettCheckedListBox.Items.Add(dealName)
+        Next
 
         'Sorting options for jarjestamineComboBox
         jarjestamineComboBox.Items.Add("A-Z")
+        jarjestamineComboBox.Items.Add("Z-A")
         jarjestamineComboBox.Items.Add("Hind langev")
         jarjestamineComboBox.Items.Add("Hind tõusev")
+
+        'Show clear packageChart on form load.
+        packageChart.Series.Clear()
+
     End Sub
 
     Private Sub jarjestamineComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles jarjestamineComboBox.SelectedIndexChanged
@@ -17,11 +36,66 @@
 
         'Sort the items in CheckedListBox based on selected sorting option
         If selectedSortOption = "A-Z" Then
+            'Sort pakettCheckedListBox
             pakettCheckedListBox.Sorted = True
-        ElseIf selectedSortOption = "Hind langev" Then
+
+        ElseIf selectedSortOption = "Z-A" Then
+            'Set sorting option to false so that it's possible to reverse sort
+            pakettCheckedListBox.Sorted = False
+
+            'Make new list of strings
+            Dim items As New List(Of String)
+
+            'Add all items in pakettCheckedListBox to newly created list of strings
+            For Each item As Object In pakettCheckedListBox.Items
+                items.Add(item.ToString())
+            Next
+
+            'Sort all items in list, then reverse the list
+            items.Sort()
+            items.Reverse()
+
+            'Clear all items in pakettCheckedListBox and then add the items back in in reverse order
+            pakettCheckedListBox.Items.Clear()
+            For Each item As String In items
+                pakettCheckedListBox.Items.Add(item)
+            Next
 
         ElseIf selectedSortOption = "Hind tõusev" Then
+            'Set sorting option to false so that it's possible to sort
+            pakettCheckedListBox.Sorted = False
 
+            'List to hold dictionary entries.
+            Dim dicList As List(Of KeyValuePair(Of String, Double)) = deals.ToList
+
+            'Sort the list by double value in ascending order.
+            dicList.Sort(Function(x, y) x.Value.CompareTo(y.Value))
+
+            'Clear pakettCheckedListBox.
+            pakettCheckedListBox.Items.Clear()
+
+            'Add sorted keys to pakettCheckedListBox.
+            For Each kvp As KeyValuePair(Of String, Double) In dicList
+                pakettCheckedListBox.Items.Add(kvp.Key)
+            Next
+
+        ElseIf selectedSortOption = "Hind langev" Then
+            'Set sorting option to false so that it's possible to sort
+            pakettCheckedListBox.Sorted = False
+
+            'List to hold dictionary entries.
+            Dim dicList As List(Of KeyValuePair(Of String, Double)) = deals.ToList
+
+            'Sort the list by double value in ascending order.
+            dicList.Sort(Function(x, y) y.Value.CompareTo(x.Value))
+
+            'Clear pakettCheckedListBox.
+            pakettCheckedListBox.Items.Clear()
+
+            'Add sorted keys to pakettCheckedListBox.
+            For Each kvp As KeyValuePair(Of String, Double) In dicList
+                pakettCheckedListBox.Items.Add(kvp.Key)
+            Next
         End If
     End Sub
 
@@ -36,43 +110,27 @@
         Me.Hide()
     End Sub
 
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    Private Sub compareButton_Click(sender As Object, e As EventArgs) Handles compareButton.Click
+        'Clear previous data.
+        packageChart.Series.Clear()
 
-    End Sub
+        'Go through checked items in pakettCheckedListBox.
+        For Each item In pakettCheckedListBox.CheckedItems
+            Dim key As String = DirectCast(item, String)
+            Dim value As Double = deals(key)
 
-    Private Sub vordlusButton_Click(sender As Object, e As EventArgs) Handles vordlusButton.Click
+            'Create a new series in the chart for each checked package.
+            Dim series As New DataVisualization.Charting.Series()
+            series.Name = key
 
-    End Sub
+            'Add data point to series with given value.
+            series.Points.AddXY(0, value)
 
-    Private Sub otsingButton_Click(sender As Object, e As EventArgs) Handles otsingButton.Click
+            'Add the series to chart.
+            packageChart.Series.Add(series)
 
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
-
-    End Sub
-
-    Private Sub CheckedListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox2.SelectedIndexChanged
-
-    End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-    End Sub
-
-    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-    End Sub
-
-    Private Sub pakettCheckedListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles pakettCheckedListBox.SelectedIndexChanged
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
+            'Add tooltip for data point to display name and price.
+            series.Points(0).ToolTip = key & " - " & Math.Round(value, 2).ToString() & " s"
+        Next
     End Sub
 End Class
